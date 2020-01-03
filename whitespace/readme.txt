22557fe20320ba9aacedeb7fbeb725ca0c5d9135
Person <zemlja-trp@yandex.com>
2020-01-03 13:55:59 +0100

    Added whitespace directory containing spacing1.py and readme.txt

---
+++ whitespace/readme.txt
@@ -0,0 +1,4 @@
+Substitutes whitespaces with underscores for image files ending with a [".png", ".jpg", ".jpeg"]
+extension.
+
+Run it from the current directory.
---
+++ whitespace/spacing1.py
@@ -0,0 +1,19 @@
+#Inefficient, reruns itself for every instance of a whitespace in every item.
+#A more efficient way would be to remove every whitespace instance for every item.
+
+img_types = [
+    ".png", ".jpg", ".jpeg"
+]
+
+def remove_whitespace():
+    for img_type in img_types:
+        for item in os.listdir("."):
+            if img_type in item:
+                for i in item:
+                    if ord(i) == 32:
+                        try:
+                            os.rename(item, item.replace(" ", "_"))
+                            remove_whitespace()
+                        except FileNotFoundError as e:
+                            print(f"Excepted {e} for {item}. Renaming ok.")
+remove_whitespace()
